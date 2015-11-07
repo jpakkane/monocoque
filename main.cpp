5042bebda4d4f31e95b764dd5f240f71c8ccb310
Jussi Pakkanen <jpakkane@gmail.com>
2015-11-07 19:22:49 +0200

    Animate item.

--- main.cpp
+++ main.cpp
@@ -23,11 +23,16 @@
 #include<cstdlib>
 #include<memory>
 #include<cassert>
+#include<math.h>
 #include"blue.h"
 
 const int SCREEN_WIDTH = 1920/2;
 const int SCREEN_HEIGHT = 1080/2;
 const uint32_t FTIME = (1000/60);
+
+// For simplicity.
+const int texw = 100;
+const int texh = 100;
 
 SDL_Texture* unpack(SDL_Renderer *rend, const unsigned char* data, size_t data_size) {
     auto *io = SDL_RWFromConstMem(data, data_size);
@@ -44,15 +49,17 @@ struct resources {
     }
 };
 
-void render(SDL_Renderer *rend, const resources &res) {
+void render(SDL_Renderer *rend, const resources &res, const double ratio) {
     SDL_Rect r;
     assert(!SDL_SetRenderDrawColor(rend, 0, 0, 0, 0));
     SDL_RenderClear(rend);
     assert(!SDL_SetRenderDrawColor(rend, 255, 255, 255, 0));
-    r.x = 100;
-    r.y = 100;
-    r.w = 100;
-    r.h = 100;
+    int centerx = SCREEN_WIDTH/2;
+    int centery = SCREEN_HEIGHT/2;
+    r.x = centerx - texw/2 + SCREEN_WIDTH*0.4*sin(2*M_PI*ratio);
+    r.y = centery - texh/2 + SCREEN_HEIGHT*0.4*cos(2*2*M_PI*(ratio) + M_PI/2);
+    r.w = texw;
+    r.h = texh;
     assert(!SDL_RenderCopy(rend, res.blue_tex.get(), nullptr, &r));
     SDL_RenderPresent(rend);
 }
@@ -60,6 +67,8 @@ void render(SDL_Renderer *rend, const resources &res) {
 void mainloop(SDL_Window *win, SDL_Renderer *rend) {
     SDL_Event e;
     resources res(rend);
+    auto start_time = SDL_GetTicks();
+    const int cycle = 2000;
     auto last_frame = SDL_GetTicks();
     SDL_RendererInfo f;
     SDL_GetRendererInfo(rend, &f);
@@ -78,7 +87,7 @@ void mainloop(SDL_Window *win, SDL_Renderer *rend) {
                 return;
             }
         }
-        render(rend, res);
+        render(rend, res, ((SDL_GetTicks() - start_time) % cycle)/double(cycle));
         if(!has_vsync) {
             auto time_spent = SDL_GetTicks() - last_frame;
             if(time_spent < FTIME) {
